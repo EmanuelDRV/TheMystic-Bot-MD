be1a75c30f09e27ef762b37e9a0be2f4d1ec3dd8
EmanuelDRV <119828411+EmanuelDRV@users.noreply.github.com>
2022-12-04 23:03:22 -0400

    config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['59160700832', '👑 Mystic - Creador 👑', true],
   ['Unptoadrih15', '👑 Mystic - Collaborator 1 👑', true],
-  ['50499698072', '👑 Mystic - Collaborator 2 👑', true],
+  ['59174578253', '👑 Mystic - Collaborator 2 👑', true],
   ['51940617554', '👑 Mystic - Collaborator 3 👑', true],
   ['51996089079', '👑 Mystic - Collaborator 4 👑', true],
   ['573225236629', '👑 Mystic - Collaborator 5 👑', true],
