0cef2b13fb074d082c423f0364c2f4e9d5efacd4
EmanuelDRV <119828411+EmanuelDRV@users.noreply.github.com>
2023-04-17 20:17:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
+  ['59174578253', '👑 Mystic - Creador 👑', true],
+  ['59174578253', '💫 Mystic - Collaborator 1 💫', true],
   ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
